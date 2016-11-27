06be9bb2b09e7648e2552ea763103e390a5de60b
Walter Dolce <walterdolce@gmail.com>
2016-11-27 17:52:36 +0000

    Add telnet.netkit to InSpec command checks

--- controls/telnet_client.rb
+++ controls/telnet_client.rb
@@ -6,12 +6,14 @@ title 'telnet client'
 control 'telnet-client' do
   impact 0.5
   title 'Prevent use of the telnet client'
-  desc 'Verifies whether there is a "telnet" client executable available in $PATH'
+  desc 'Verifies whether a telnet client executable is available in PATH'
   
   tag 'production', 'development'
   tag 'telnet', 'protocol'
   
-  describe command('telnet') do
-    it { should_not exist }
+  %w(telnet telnet.netkit).each do |telnet_executable|
+    describe command(telnet_executable) do
+      it { should_not exist }
+    end
   end
 end
